f4be70135899e3b97037bc523fdc58b6aca2802e
mudabbir-alam <mudabbiralam885@gmail.com>
2026-01-31 16:33:57 +0500

    EmailJS Configured and Contact Form is functional now

--- src/components/Main/Contact.jsx
+++ src/components/Main/Contact.jsx
@@ -1,7 +1,42 @@
 import React from "react";
 import Button from "./Button";
+import emailjs from "emailjs-com";
+import { useState } from "react";
 
 function Contact() {
+    const [formData, setFormData] = useState({
+        first_name: "",
+        last_name: "",
+        email: "",
+        message: "",
+    });
+
+    function handleChange(e) {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value,
+        });
+    }
+
+    function handleSubmit(e) {
+        e.preventDefault();
+
+        emailjs
+        .send(
+            import.meta.env.VITE_EMAIL_SERVICE_ID,
+            import.meta.env.VITE_EMAIL_TEMPLATE_ID,
+            formData,
+            import.meta.env.VITE_EMAIL_PUBLIC_KEY
+        )
+        .then(() => {
+            alert("Message Sent Successfully");
+            setFormData({first_name: "", last_name: "", email: "", message: "",});
+        })
+        .catch(() => {
+            alert("Failed to send the message");
+        });
+    }
+
     return (
         <section id="contact" className="py-16">
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8 p-6 md:p-12 border border-border rounded-2xl bg-primary/5 dark:bg-primary/20">
@@ -28,24 +63,30 @@ function Contact() {
                         </span>
                     </div>
                 </div>
-                <form className="contact-right grid grid-cols-2 gap-4">
+                <form className="contact-right grid grid-cols-2 gap-4" onSubmit={handleSubmit}>
                     <input
                         type="text"
                         name="first_name"
+                        value={formData.first_name}
                         id="first_name"
+                        onChange={handleChange}
                         placeholder="First name"
                         className="bg-background dark:text-slate-300 p-3 border-2 border-border rounded-lg focus:border-primary outline-none col-span-2 sm:col-span-1"
                     />
                     <input
                         type="text"
                         name="last_name"
+                        value={formData.last_name}
                         id="last_name"
+                        onChange={handleChange}
                         placeholder="Last Name"
                         className="bg-background dark:text-slate-300 p-3 border-2 border-border rounded-lg focus:border-primary outline-none col-span-2 sm:col-span-1"
                     />
                     <input
                         type="email"
                         name="email"
+                        value={formData.email}
+                        onChange={handleChange}
                         id="email"
                         placeholder="Email Address"
                         className="col-span-2 bg-background dark:text-slate-300 p-3 border-2 border-border rounded-lg focus:border-primary outline-none"
@@ -53,12 +94,14 @@ function Contact() {
                     />
                     <textarea
                         name="message"
+                        value={formData.message}
                         id="message"
+                        onChange={handleChange}
                         placeholder="Your message"
                         rows="4"
                         className="col-span-2 bg-background dark:text-slate-300 p-3 border-2 border-border rounded-lg focus:border-primary outline-none"
                     ></textarea>
-                    <Button label="Send Message" className="bg-primary text-white hover:bg-primary/90 col-span-2"/>
+                    <Button label="Send Message" className="bg-primary text-white hover:bg-primary/90 col-span-2" type="submit"/>
                 </form>
             </div>
         </section>
