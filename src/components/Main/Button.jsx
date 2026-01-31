36e3a3e4ef1c166958149db854a3bb3db3b94723
mudabbir-alam <mudabbiralam885@gmail.com>
2026-01-31 16:56:28 +0500

    EmailJS Configured and Contact Form is functional now

--- src/components/Main/Button.jsx
+++ src/components/Main/Button.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-function Button({label= "Label", className = "", type = "button"}) {
+function Button({label= "Label", className = "", type = "submit"}) {
   return (
     <button className={`py-4 px-8 rounded-lg text-base font-medium hover:cursor-pointer ${className}`} type={type}>{label}</button>
   );
