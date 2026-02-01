d40a401c264b8d6e4a6eeffdc88deb43f2c0101f
mudabbir-alam <mudabbiralam885@gmail.com>
2026-02-01 18:45:27 +0500

    Made logo a hyperlink to the top of page

--- src/components/Main/Sidebar.jsx
+++ src/components/Main/Sidebar.jsx
@@ -12,7 +12,7 @@ function Sidebar() {
             className={`w-4/5 h-screen fixed top-0 right-0 bg-white dark:bg-slate-800 z-30 flex flex-col gap-8 ${sidebar.isSidebarOpen ? "translate-x-0" : "translate-x-full"} transition-transform ease-in-out border-l border-border`}
         >
             <div className="border-b border-border h-16 w-full flex justify-between items-center px-6">
-                <Logo />
+                <Logo onClick={sidebar.toggleSidebar}/>
                 <button
                     className="material-symbols-outlined leading-none p-2 bg-slate-200 hover:bg-slate-300 dark:hover:bg-slate-700 border-border border dark:bg-background rounded-lg text-xl font-bold text-foreground cursor-pointer"
                     onClick={sidebar.toggleSidebar}
