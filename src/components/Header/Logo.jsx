deec5a39ced8b89cd72642ac9671be8b77609282
mudabbir-alam <mudabbiralam885@gmail.com>
2026-02-01 21:57:13 +0500

    Minor bug fixed

--- src/components/Header/Logo.jsx
+++ src/components/Header/Logo.jsx
@@ -3,12 +3,8 @@ import React from "react";
 function Logo({onClick}) {
     return (
         <a onClick={onClick} href="#" className="logo-container flex justify-center items-center gap-2 text-base">
-            <div className="bg-primary text-white p-1.5 flex items-center justify-center rounded-lg">
-                <span className="material-symbols-outlined text-xl leading-none">
-                    code
-                </span>
-            </div>
-            <span className="title font-bold text-lg tracking-tight">Mudabbir Alam</span>
+            <span className="material-symbols-outlined text-lg bg-primary p-2 leading-none font-bold rounded-lg text-white" >code</span>
+            <span className="title font-bold text-lg tracking-tight text-foreground">Mudabbir Alam</span>
         </a>
     );
 }
