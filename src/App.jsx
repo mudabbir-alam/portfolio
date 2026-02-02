8c4f7fff3686c1b4c88ce316ddd6891b9ad059a4
mudabbir-alam <mudabbiralam885@gmail.com>
2026-02-02 23:04:13 +0500

    Loading Screen Added

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import "./App.css";
+// import "./App.css";
 import Header from "./components/Header/Header.jsx";
 import Main from "./components/Main/Main.jsx";
 import Footer from "./components/Footer/Footer.jsx";
@@ -9,32 +9,56 @@ export const SidebarContext = createContext();
 
 function App() {
     const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+    const [isLoading, setIsLoading] = useState(true);
+
     useEffect(() => {
-        const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
+        async function loadResources() {
+            try {
+                console.log("Loading...");
+                await Promise.all([
+                    document.fonts.load('400 1em "Material Icons"'),
+                    document.fonts.load('500 1em "Material Icons"'),
+                    document.fonts.load('700 1em "Material Icons"'),
+                ]);
+                console.log("Fonts Loaded");
+                setIsLoading(false);
+            } catch (error) {
+                console.error("Error", error);
+            }
+        }
+        loadResources();
+    }, []);
+
+    useEffect(() => {
+        const scrollbarWidth =
+            window.innerWidth - document.documentElement.clientWidth;
 
         if (isSidebarOpen) {
-        document.body.style.overflow = "hidden";
-        document.body.style.paddingRight = `${scrollbarWidth}px`;
-        }
-        else {
+            document.body.style.overflow = "hidden";
+            document.body.style.paddingRight = `${scrollbarWidth}px`;
+        } else {
             document.body.style.overflow = "auto";
             document.body.style.paddingRight = "";
         }
-    }, [isSidebarOpen])
+    }, [isSidebarOpen]);
 
     function toggleSidebar() {
         setIsSidebarOpen((i) => !i);
     }
 
+    if (isLoading) {
+        return <div>Loading...</div>;
+    }
+
     return (
         <SidebarContext.Provider value={{ isSidebarOpen, toggleSidebar }}>
             <>
-            <SidebarOverlay />
-            <div id="body" className="w-full min-h-screen font-inter">
-                <Header />
-                <Main />
-                <Footer />
-            </div>
+                <SidebarOverlay />
+                <div id="body" className="w-full min-h-screen font-inter">
+                    <Header />
+                    <Main />
+                    <Footer />
+                </div>
             </>
         </SidebarContext.Provider>
     );
